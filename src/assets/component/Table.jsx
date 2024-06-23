854a5ee5b6323b50e08e8635c05d1b5c3b1be38b
niraj <nirajhoichoi@gmail.com>
2024-06-24 02:36:22 +0530

    complete 3

--- src/assets/component/Table.jsx
+++ src/assets/component/Table.jsx
@@ -1,8 +1,11 @@
-import React, { useMemo } from "react";
+import React from "react";
+import { useMemo } from "react";
 import {
   MaterialReactTable,
   useMaterialReactTable,
 } from "material-react-table";
+
+import { useReactTable } from "@tanstack/react-table";
 
 import sample_data from "../../../sample_data.json";
 
@@ -27,69 +30,71 @@ function formatDate(dateString) {
 }
 
 const Table = () => {
-  const columns = useMemo(
-    () => [
-      {
-        header: "ID",
-        accessorKey: "id",
+  const data = useMemo(() => sample_data, []);
+
+  const columns = [
+    {
+      header: "ID",
+      accessorKey: "id",
+    },
+    {
+      header: "Name",
+      accessorKey: "name",
+    },
+    {
+      header: "Category",
+      accessorKey: "category",
+      filterVariant: "multi-select",
+    },
+    {
+      header: "Subcategory",
+      accessorKey: "subcategory",
+      filterVariant: "multi-select",
+    },
+    {
+      header: "CreatedAt",
+      id: "createdAt",
+      filterVariant: "date-range",
+
+      accessorFn: (originalRow) => new Date(originalRow.createdAt), //convert to date for sorting and filtering
+      Cell: ({ cell }) => formatDate(cell.getValue()), // convert back to string for display
+    },
+    {
+      header: "UpdatedAt",
+      id: "updatedAt",
+      filterVariant: "date-range",
+
+      accessorFn: (originalRow) => new Date(originalRow.updatedAt), //convert to date for sorting and filtering
+      Cell: ({ cell }) => formatDate(cell.getValue()), // convert back to string for display
+    },
+    {
+      header: "Price",
+      accessorKey: "price",
+      filterVariant: "range-slider",
+      filterFn: "betweenInclusive",
+      muiFilterSliderProps: {
+        marks: true,
+        min: 11,
+        max: 200,
       },
-      {
-        header: "Name",
-        accessorKey: "name",
+    },
+    {
+      header: "Sale price",
+      accessorKey: "sale_price",
+      filterVariant: "range-slider",
+      filterFn: "betweenInclusive",
+      muiFilterSliderProps: {
+        marks: true,
+        min: 0,
+        max: 100,
       },
-      {
-        header: "Category",
-        accessorKey: "category",
-        filterVariant: "multi-select",
-      },
-      {
-        header: "Subcategory",
-        accessorKey: "subcategory",
-        filterVariant: "multi-select",
-      },
-      {
-        header: "CreatedAt",
-        id: "createdAt",
-        filterVariant: "date-range",
-        accessorFn: (originalRow) => new Date(originalRow.createdAt), //convert to date for sorting and filtering
-        Cell: ({ cell }) => formatDate(cell.getValue()), // convert back to string for display
-      },
-      {
-        header: "UpdatedAt",
-        id: "updatedAt",
-        filterVariant: "date-range",
-        accessorFn: (originalRow) => new Date(originalRow.updatedAt), //convert to date for sorting and filtering
-        Cell: ({ cell }) => formatDate(cell.getValue()), // convert back to string for display
-      },
-      {
-        header: "Price",
-        accessorKey: "price",
-        filterVariant: "range-slider",
-        filterFn: "betweenInclusive",
-        muiFilterSliderProps: {
-          marks: true,
-          min: 11,
-          max: 200,
-        },
-      },
-      {
-        header: "Sale price",
-        accessorKey: "sale_price",
-        filterVariant: "range-slider",
-        filterFn: "betweenInclusive",
-        muiFilterSliderProps: {
-          marks: true,
-          min: 0,
-          max: 100,
-        },
-      },
-    ],
-    []
-  );
+    },
+  ];
 
   const table = useMaterialReactTable({
-    sample_data,
+    data,
     columns,
+
     enableFacetedValues: true,
     initialState: { showColumnFilters: true },
   });
